58343f355e5e6a4cff3ee7d747d7188d96c7a35b
Jorge Quintero <jorge.quintero@prestti.com>
2022-07-12 13:55:55 -0500

    Created Home Screeen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image } from 'react-native'
+import Colors from '../constants/Colors'
+import CbButton from '../components/CbButton'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+<SafeAreaView style={styles.container}>
+  <ScrollView
+    contentContainerStyle={{alignItems: 'center'}}
+  >
+    <Image 
+      style={styles.image} 
+      source={{uri:'https:i.imgur.com/9EEaSaS.png'}}
+    />
+    <Text style={styles.title}>Welcome to Coinbase!</Text>
+    <Text style={styles.subtitle}>Make your first investment today</Text>
+    <CbButton title='Get Started' />
+  </ScrollView>
+</SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'flex-start',
+    backgroundColor:'#FFFFFF',
+  },
+  image:{
+    height: 250,
+    width:150,
+    marginTop: 40,
+  },
+  title: {
+    fontSize:21,
+    fontWeight:'600',
+    marginBottom: 8,
+    letterSpacing: 0.5,
+  },
+  subtitle: {
+    fontSize:17,
+    marginBottom:24,
+    color: Colors.subtitle, 
+  },
+})
